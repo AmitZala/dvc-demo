72f1013f207835f4b60fe7eae29bcb3549d41b86
AmitZala <amitzala@live.com>
2024-10-10 18:09:28 +0530

    second commit

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -8,6 +8,7 @@ df = pd.read_csv('D:\Vs code\dvc-demo\data\Ecommerce Customers.csv')
 
 df = df.iloc[:, 3:]
 
-df = df[df['Length of Membership'] > 3]
+df = df[df['Length of Membership'] > 1]
+df.drop(columns=['Avg. Session Length'], inplace=True)
 
 df.to_csv(os.path.join('data','customers.csv'))
